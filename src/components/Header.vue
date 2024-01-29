1cbdbacbd223c1a8a31328e0b3a49be92f32cd37
GytisMil <gytis.miliauskas963@gmail.com>
2024-01-29 11:12:21 +0200

    Added placeholder function to buttons; Disabled user select on the buttons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,17 @@
 <script setup>
+function name(event) {
+  if(event) {
+    alert(event.target.innerHTML)
+  }
+}
 </script>
 
 <template>
-  <div>
+  <div @click="name">
   Register
+  </div>
+  <div @click="name">
+  Log In
   </div>
 </template>
 <style scoped>
