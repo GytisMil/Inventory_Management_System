38f0adfa6b0344964ef2bcd02a8f82fc22da9d4e
GytisMil <gytis.miliauskas963@gmail.com>
2024-02-07 10:48:31 +0200

    Added template page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from './components/Header.vue'
 import Inventory from './components/Inventory.vue'
+import About from './components/About.vue'
 </script>
 
 <template>
---
+++ src/components/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quia
+aperiam eligendi dolorum reprehenderit ea amet, aliquid dolorem beatae,
+iste aliquam ullam. Sequi ab eligendi consectetur neque laudantium,
+libero asperiores.
+</template>
+<style scoped>
+</style>
